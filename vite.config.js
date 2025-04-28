9147b153db55f44440279b4a2a88049e1dbe082e
Lindon Thomas <lindon@theinfinitemachine.net>
2025-04-28 04:50:11 +0000

    New build.yaml, updated configs to not have to manually copy files over simply run // build and it will copy over the files to the dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: "manifest.json", dest: "." },
+        { src: "background.js", dest: "." },
+        { src: "content.js", dest: "." },
+        { src: "icons", dest: "." } // Copy whole /icons folder
+      ]
+    })
+  ],
   build: {
+    outDir: "dist",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         popup: resolve(__dirname, "popup/public/index.html"),
-        options: resolve(__dirname, "options/public/index.html"),
+        options: resolve(__dirname, "options/public/index.html")
       },
       output: {
         entryFileNames: "assets/[name].js",
         chunkFileNames: "assets/[name].js",
         assetFileNames: "assets/[name].[ext]"
       }
-    },
-    outDir: "dist",
-    emptyOutDir: true
+    }
   }
 });
